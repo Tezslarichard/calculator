dec00ef3b76d9bba2824af1dfdf48b92c788004d
Balint Aranyi <aranyi.balint@gmail.com>
2024-09-28 10:58:45 +0200

    Base JS

---
+++ main.js
@@ -0,0 +1,24 @@
+// Kijelző elem lekérése
+const display = document.getElementById("display"); 
+
+// Bevitel hozzáadása a kijelzőhöz
+function appendToDisplay(input){
+    display.value += input;
+}
+
+// Kijelző törlése
+function clearDisplay(){
+    display.value = "";
+}
+
+// Kifejezés kiszámítása
+function calculate(){
+    try{
+        display.value = eval(display.value);  // Eredmény kiiratása
+    }
+    catch(error) {
+        display.value = "Error"; // Hiba esetén
+    }
+}
+
+// Négyzetre emelés
